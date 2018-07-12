2d8ab97bec8fdd049521bbf6c2f64d0b7a2c10b1
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-07-12 15:25:10 -0400

    update has many through

--- week-3/orm-has-many-through/moes.py
+++ week-3/orm-has-many-through/moes.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Text, ForeignKey, String
+from sqlalchemy import Column, Integer, Text, ForeignKey, String, Boolean
 
 from sqlalchemy.orm import sessionmaker, relationship, backref
 import sqlalchemy
@@ -12,16 +12,22 @@ class Bartender(Base):
     name = Column(Text)
     hometown = Column(Text)
     birthyear = Column(Integer)
-    customers = relationship("Customer", secondary="orders", back_populates="bartenders")
 
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(Integer, primary_key = True)
-    customer = relationship("Customer")
     customer_id = Column(Integer, ForeignKey('customers.id'))
     bartender_id = Column(Integer, ForeignKey('bartenders.id'))
-    bartender = relationship(Bartender)
     drink_id = Column(Integer, ForeignKey('drinks.id'))
+
+class Drink(Base):
+    __tablename__ = 'drinks'
+    id = Column(Integer, primary_key = True)
+    name = Column(Text)
+    calories = Column(Text)
+    price = Column(Text)
+    alcoholic = Column(Boolean)
+
 
 class Customer(Base):
     __tablename__ = 'customers'
@@ -29,7 +35,7 @@ class Customer(Base):
     name = Column(Text)
     hometown = Column(Text)
     birthyear = Column(Integer)
-    bartenders = relationship(Bartender, secondary="orders", back_populates="customers", lazy='dynamic')
+
 
 engine = sqlalchemy.create_engine('sqlite:///moes.db', echo=True)
 Base.metadata.create_all(engine)
