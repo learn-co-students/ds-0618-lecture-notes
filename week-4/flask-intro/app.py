e3c63096c455d5bb2a93bea8fe41c126126563c2
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-07-16 14:20:05 -0400

    flask intro

--- week-4/flask-intro/app.py
+++ week-4/flask-intro/app.py
@@ -6,11 +6,23 @@ app = Flask(__name__)
 
 @app.route('/nba/teams')
 def index():
+    # response = app.make_response(('<h1> whatever <h1>', 200))
+    # , 203, {'content-type': 'text/html'}
     return render_template('index.html')
 
 @app.route('/nba/teams/<id>')
-def show(id):
-    team = db.session.query(Team).get(id)
-    return render_template('show.html', team=team)
+def show(team):
+    # return team
+    return render_template('show.html', team_name=team)
+
+
+# def show_sixers():
+#     'here we are'
+#     pdb.set_trace()
+#     pass
+
+# app.add_url_rule('/nba/teams', 'index', index)
+# app.add_url_rule('/team/sixers', 'bob', show_bob)
+
 
 app.run(debug=True)
