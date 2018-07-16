2c9aa17c09aeb688344ae55a492e3a526f874edb
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-07-16 01:20:02 -0400

    update craigslist

--- week-3/scraping/craigslist.py
+++ week-3/scraping/craigslist.py
@@ -1,5 +1,6 @@
 import bs4
 import requests
+from console import session
 
 class CraigsList:
     def __init__(self):
@@ -7,11 +8,26 @@ class CraigsList:
         self.soup = bs4.BeautifulSoup(self.text)
 
     def apartment_rows(self):
+        # <li>
         return self.soup.find_all(class_="result-row")
 
-    def apartment(self):
-        return self.apartment_rows()[0]
+    def create_craigslist_apts(self):
+        apt_rows = []
+        for apt_row in self.apartment_rows():
+            cla = CraigsListApartment(apt_row)
+            apt_rows.append(cla)
+            apt = Apartment(price=cla.price(), title=cla.title())
+            session.add(apt)
+        session.commit()
+        return apt_rows
+
+class CraigsListApartment:
+    def __init__(self, result_row):
+        self.apt_soup = result_row
 
     def price(self):
-        price_string = self.apartment().find(class_='result-price').text
+        price_string = self.apt_soup.find(class_='result-price').text
         return int(price_string[1:])
+
+    def title(self):
+        return self.apt_soup.find(class_="result-title hdrlnk").text
